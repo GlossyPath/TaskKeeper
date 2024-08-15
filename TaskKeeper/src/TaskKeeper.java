6bc723a25e4719b18a55c15d29b584412c170f4e
GlossyPath <glossypath0808@gmail.com>
2024-08-15 19:59:05 +0200

    comentario javadoc

--- TaskKeeper/src/TaskKeeper.java
+++ TaskKeeper/src/TaskKeeper.java
@@ -1,9 +1,21 @@
-
+/**
+ * 
+ * TaskKeeper es una aplicación simple y eficiente desarrollada en Java para gestionar
+ * y guardar tus tareas diarias. Con una interfaz fácil de usar, TaskKeeper permite
+ * a los usuarios agregar, editar, y eliminar tareas, asegurándose de que nunca se pierdan
+ * las tareas importantes. Es una herramienta ideal para aquellos que buscan una solución 
+ * ligera y sin complicaciones para llevar un registro de sus tareas.
+ *@version 1.0;
+ *
+ * @since 15/08/2024;
+ * 
+ * @author GlossyPath;
+ * 
+ */
 
 public class TaskKeeper {
 
     public static void main(String[] args) {
         
-    }
-    
+    }    
 }
