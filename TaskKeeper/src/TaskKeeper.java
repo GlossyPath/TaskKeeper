210756add145e2347d532964489da2b3796bb1ba
GlossyPath <glossypath0808@gmail.com>
2024-08-15 20:08:57 +0200

    añadimos frame centrado en la pantalla del pc

--- TaskKeeper/src/TaskKeeper.java
+++ TaskKeeper/src/TaskKeeper.java
@@ -5,6 +5,7 @@
  * a los usuarios agregar, editar, y eliminar tareas, asegurándose de que nunca se pierdan
  * las tareas importantes. Es una herramienta ideal para aquellos que buscan una solución 
  * ligera y sin complicaciones para llevar un registro de sus tareas.
+ * 
  *@version 1.0;
  *
  * @since 15/08/2024;
@@ -13,9 +14,36 @@
  * 
  */
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+
 public class TaskKeeper {
 
     public static void main(String[] args) {
         
+        MyFrame frame = new MyFrame();
+
+        frame.setVisible(true);
     }    
+}
+
+class MyFrame extends JFrame {
+
+    public MyFrame(){
+
+        setTitle("Task_Keeper");
+
+        Toolkit getToolkit = Toolkit.getDefaultToolkit();    
+        Dimension size = getToolkit.getScreenSize();
+
+        int height = size.height;
+        int width = size.width;
+
+        setSize(width/2, height/2);
+        setLocation(width/4, height/4);
+
+    }
 }
